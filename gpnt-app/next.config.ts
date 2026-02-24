e02ee7485289b2de3fbccf517fba9e52c0aa9fdd
jak.u.b.p <130249082+uba1709@users.noreply.github.com>
2026-02-24 16:47:26 +0100

    Update next.config.ts

--- gpnt-app/next.config.ts
+++ gpnt-app/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/-GPNT_Web',
+  images: {
+    unoptimized: true, // Wymagane na GitHub Pages
+  },
 };
 
 export default nextConfig;
